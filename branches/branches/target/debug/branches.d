2372ffa39fc000841ba0acdcd729234a7c59395e
Bektur <zmurik797@gmail.com>
2024-01-23 01:00:11 +0600

    Retest

---
+++ branches/branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+C:\Users\Bektur\Desktop\rust\branches\branches\target\debug\branches.exe: C:\Users\Bektur\Desktop\rust\branches\branches\src\main.rs
---
+++ branches/branches/target/debug/deps/branches.d
@@ -0,0 +1,5 @@
+C:\Users\Bektur\Desktop\rust\branches\branches\target\debug\deps\branches.exe: src\main.rs
+
+C:\Users\Bektur\Desktop\rust\branches\branches\target\debug\deps\branches.d: src\main.rs
+
+src\main.rs:
